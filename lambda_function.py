c48444945322fa7b189974a016e64f411e2d7f05
Noopura Vaidya <noopuravaidya14@gmail.com>
2025-08-04 10:20:34 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -32,7 +32,7 @@ def lambda_handler(event, context):
         )
 
         return wr_response
-    except Exception as a:
-        print(a)
+    except Exception as e:
+        print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        raise a
+        raise e
